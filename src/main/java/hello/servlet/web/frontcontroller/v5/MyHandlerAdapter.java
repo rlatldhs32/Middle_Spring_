c16a8803323f71d2a32067b26752b8fd984b3d04
rlatldhs32 <rlatldhs32@naver.com>
2022-02-11 22:31:27 +0900

    220211 With hayeon, Do handling

--- src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
+++ src/main/java/hello/servlet/web/frontcontroller/v5/MyHandlerAdapter.java
@@ -1,15 +1,11 @@
 package hello.servlet.web.frontcontroller.v5;
-
 import hello.servlet.web.frontcontroller.ModelView;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
 public interface MyHandlerAdapter {
-
-    boolean supports(Object Handler); //지원가능한지
-
-    ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException;
+    boolean supports(Object handler);
+    ModelView handle(HttpServletRequest request, HttpServletResponse response,
+                     Object handler) throws ServletException, IOException;
 }
